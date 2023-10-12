369a91f22209a3f800cf74d46dbc5ecf4a8a7d92
Adriel Alcântara <adriel.sena2003@hotmail.com>
2023-10-12 16:08:36 -0300

    Update Calculadora.py

--- Calculadora Simples/Calculadora.py
+++ Calculadora Simples/Calculadora.py
@@ -1,2 +1,8 @@
-print('teste')
-print('teste1')
+def escolhaOperacao():
+    print('0 : Soma')
+    print('1 : Subtração')
+    print('2 : Multiplicação')
+    print('3 : Divisão')
+    print('4 : Exponenciação')
+
+escolhaOperacao()
